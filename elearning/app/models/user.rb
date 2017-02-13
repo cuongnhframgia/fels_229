12121400de8bcf6f17f22f95be2e1a162ce6c0a6
huyvvbka <huyvvbka@gmail.com>
2017-02-13 14:34:48 +0700

    User login

--- elearning/app/models/user.rb
+++ elearning/app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  attr_accessor :remember_token
   before_save :email_downcase
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -16,8 +17,33 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
-  private
   def email_downcase
     self.email = email.downcase
+  end
+
+  class << self  
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 end
--- elearning/config/routes.rb
+++ elearning/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
   root "pages#show", page: "home"
 
   get "/pages/*page", to: "pages#show"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
 
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
   resources :users, only: [:show]
 end

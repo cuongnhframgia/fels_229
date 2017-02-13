64a697bec239439153692869a7b13d8eeb34e8a1
huyvvbka <huyvvbka@gmail.com>
2017-02-13 09:55:34 +0700

    User signup

--- elearning/config/routes.rb
+++ elearning/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "pages#show", page: "home"
+
   get "/pages/*page", to: "pages#show"
+  get "/signup", to: "users#new"
+
+  post "/signup", to: "users#create"
+  resources :users, only: [:show]
 end
